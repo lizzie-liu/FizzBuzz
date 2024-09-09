43bd2a0b421970e8e32f46b2445577a511f1d1f4
lizzie-liu <elizabethfj.liu@mail.utoronto.ca>
2024-09-09 10:29:17 -0400

    fixed syntax errors

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,11 +1,13 @@
 public class Multiples {
     public static void main(String[] args) {
         int numMults = 0;
+
+        int i = 1;
         while (i < 1000){
             if (i % 3 == 0) {
                 numMults++;
             }
-            elif (i % 5 == 0) {
+            else if (i % 5 == 0) {
                 numMults++;
             }
             i++;
